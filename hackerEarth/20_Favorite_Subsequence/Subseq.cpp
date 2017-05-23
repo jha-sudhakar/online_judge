552fc918cd31ec30ed7f079b81fdedfa27e5b868
jha-sudhakar <jhasahj@gmail.com>
2017-05-23 17:25:56 +0530

    hacker earth sub seq problem

--- hackerEarth/20_Favorite_Subsequence/Subseq.cpp
+++ hackerEarth/20_Favorite_Subsequence/Subseq.cpp
@@ -1,7 +1,9 @@
 #include<vector>
 #include<iostream>
 using namespace std;
-#define P 1000000007
+const unsigned long long int P = 1000000007;
+
+unsigned long long int count_arr[100004];
 
 class Sseq
 {
@@ -30,8 +32,8 @@ void Sseq::read_input()
 
 void Sseq::get_count()
 {
-	unsigned int total_count = 0;
-	unsigned int s=0, e=str.size()-1;
+	unsigned long long int total_count = 0;
+	unsigned long long int s=0, e=str.size()-1;
 	
 	while(str[s] != 'a' && s<str.size())
 		s++;
@@ -43,39 +45,53 @@ void Sseq::get_count()
 		cout << (total_count%P) << endl;
 		return;
 	}
-
-	vector<unsigned int> count_arr;
+#if 0
+	vector<unsigned long long int> count_arr;
 	count_arr.resize(str.size());
-	for(unsigned int i=0; i<count_arr.size(); i++)
+	
+	for(unsigned long long int i=0; i<count_arr.size(); i++)
 	{
 		count_arr[i] = 0;
 	}
+#endif
 
 	count_arr[s] = 1;
-	for(unsigned int i=s+1; i<=e; i++)
+	for(unsigned long long int i=s+1; i<=e; i++)
 	{
-		for(unsigned int j=s; j<i; j++)
+		count_arr[i] = 0;
+		for(unsigned long long int k=i; k>s; k--)
 		{
+			unsigned long long int j=k-1;			
 			if(str[i] == 'a' && str[j] == 'a')
 			{
 				count_arr[i] += count_arr[j];
+				count_arr[i] = count_arr[i]%P;
 			}
 			else if(str[i] == 'b' && (str[j] == 'a' || str[j] == 'b'))
 			{
 				count_arr[i] += count_arr[j];
+				count_arr[i] = count_arr[i]%P;
 			}
 			else if(str[i] == 'c' && (str[j] == 'b' || str[j] == 'c'))
 			{
 				count_arr[i] += count_arr[j];
+				count_arr[i] = count_arr[i]%P;
+			}
+
+			if(str[i] == str[j])
+			{
+				count_arr[i] += count_arr[j];
+				count_arr[i] = count_arr[i]%P;
+				break;
 			}
 		}
-		if(str[i] == 'a')
-			count_arr[i]++; // It can be starting point ignoring prefix.
-		else if(str[i] == 'c')
+		if(str[i] == 'c')
 		{
 			total_count += count_arr[i];
-			cout <<"Index= " << i <<", count= " << count_arr[i] << endl;
+			total_count = total_count%P;
+			//cout <<"Index= " << i <<", count= " << count_arr[i] << endl;
 		}
+		//cout <<"Index= " << i <<",char= " << char(str[i]) <<",  count= " << count_arr[i] << endl;
 	}
 
 	cout << (total_count%P) << endl;
